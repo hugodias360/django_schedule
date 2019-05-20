0bfe05841ef03782c9c3e3f904d9ac7087e63687
hugodias360 <hugodias360@gmail.com>
2019-05-20 19:49:30 -0300

    Update settings.py

--- django_schedule/settings.py
+++ django_schedule/settings.py
@@ -137,9 +137,8 @@ MIDDLEWARE_CLASSES = (
 )
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static", "static" )
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
